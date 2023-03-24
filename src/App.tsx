145a8fc29dc3cc8393915178875d1ee8e17ade34
Nafis Handoko <naffisioner@gmail.com>
2023-03-24 14:52:38 +0700

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
-function Question(props:{text:string}) {
+function Question(props: { text: string }) {
   return (
-    <div className='p-7 px-64'>
+    <div className='p-7 px-44'>
       <div className='flex flex-row items-start container mx-auto gap-4'>
         <i className="bi bi-question-circle-fill text-2xl"></i>
         <p>{props.text}</p>
@@ -14,9 +14,9 @@ function Question(props:{text:string}) {
   )
 }
 
-function Answer(props:{text:string}) {
+function Answer(props: { text: string }) {
   return (
-    <div className='bg-violet-600 p-7 px-64'>
+    <div className='bg-violet-600 p-7 px-44'>
       <div className='flex flex-row items-start container mx-auto gap-4'>
         <i className="bi bi-robot text-2xl"></i>
         <p>{props.text}</p>
@@ -37,10 +37,40 @@ function App() {
     text: string,
     type: string
   }
-  const [chats, setChats] = useState<Chats[]>([])
 
-  const handleSubmit = async (e:FormEvent) => {
+  const dummyChats: Chats[] = [
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia qui distinctio adipisci iusto quidem saepe mollitia nulla fugiat exercitationem, totam dolores asperiores et aperiam quae consequatur sunt consectetur sed fuga sequi labore voluptatum earum perferendis?',
+      type: TextType.QUESTION
+    },
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia qui distinctio adipisci iusto quidem saepe mollitia nulla fugiat exercitationem, totam dolores asperiores et aperiam quae consequatur sunt consectetur sed fuga sequi labore voluptatum earum perferendis?',
+      type: TextType.ANSWER
+    },
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia qui distinctio adipisci iusto quidem saepe mollitia nulla fugiat exercitationem, totam dolores asperiores et aperiam quae consequatur sunt consectetur sed fuga sequi labore voluptatum earum perferendis?',
+      type: TextType.QUESTION
+    },
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia qui distinctio adipisci iusto quidem saepe mollitia nulla fugiat exercitationem, totam dolores asperiores et aperiam quae consequatur sunt consectetur sed fuga sequi labore voluptatum earum perferendis?',
+      type: TextType.ANSWER
+    },
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia qui distinctio adipisci iusto quidem saepe mollitia nulla fugiat exercitationem, totam dolores asperiores et aperiam quae consequatur sunt consectetur sed fuga sequi labore voluptatum earum perferendis?',
+      type: TextType.QUESTION
+    },
+    {
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia qui distinctio adipisci iusto quidem saepe mollitia nulla fugiat exercitationem, totam dolores asperiores et aperiam quae consequatur sunt consectetur sed fuga sequi labore voluptatum earum perferendis?',
+      type: TextType.ANSWER
+    }
+  ]
+
+  const [chats, setChats] = useState<Chats[]>([])
+  const chatsContainer = useRef(null)
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
+    // window.scrollTo(0, document.body.scrollHeight)
     const tempChats = [...chats, {
       text: input,
       type: TextType.QUESTION
@@ -64,34 +94,85 @@ function App() {
         type: TextType.ANSWER
       }])
       console.log('openai answer: ' + parsedData)
+      // window.scrollTo(0, document.body.scrollHeight)
     }
 
   }
 
-  const testSubmit = async (e:FormEvent) => {
+  const testSubmit = async (e: FormEvent) => {
     e.preventDefault()
     console.log('submit succeed')
+    const tempChats = [...chats, {
+      text: input,
+      type: TextType.QUESTION
+    }]
+    setChats(tempChats)
+    setInput('')
+    window.scrollTo(0, document.body.scrollHeight)
   }
 
+  // return (
+  //   <div className={`text-white ${chats.length ? 'pb-20' : ''}`}>
+  //     {chats.length ? <div className='' ref={chatsContainer}>
+  //       {chats && chats.map((chat, index) => (
+  //         <div key={index}>
+  //           {chat.type == TextType.QUESTION ? <Question text={chat.text} /> : <Answer text={chat.text} />}
+  //         </div>
+  //       ))}
+  //     </div> : <div className='container mx-auto flex flex-col items-center justify-center h-screen w-full text-center'>
+  //       <i className="bi bi-robot text-3xl mb-4"></i>
+  //       <h1 className='text-2xl font-bold'>Welcome to NeuralBot</h1>
+  //       <h1 className='text-2xl font-bold'>You can ask anything here</h1>
+  //       <span className='mt-4'>Made with ❤️ by Nafis Handoko</span>
+  //     </div>}
+  //     <div className='fixed bottom-5 w-full flex flex-col items-center'>
+  //       <form onSubmit={testSubmit} className="relative w-3/5">
+  //         <div className="flex absolute inset-y-0 left-0 items-center pl-5 pointer-events-none">
+  //           <i className="bi bi-question-circle-fill text-violet-700"></i>
+  //         </div>
+  //         <input value={input} onChange={(e) => setInput(e.target.value)} type="text" id="simple-search" className="shadow-lg bg-white text-violet-700 border border-violet-600 text-sm rounded-lg focus:ring-violet-700 focus:border-violet-700 block w-full px-14 p-2.5 placeholder-gray-500" placeholder="Ask a question..." />
+  //         <button type='submit' className="flex absolute inset-y-0 right-0 items-center pr-5 cursor-pointer">
+  //           <i className="bi bi-send-fill text-violet-700"></i>
+  //         </button>
+  //       </form>
+  //     </div>
+  //     <button className='fixed top-5 left-5 p-3 bg-white text-black rounded' onClick={() => window.scrollTo(0, document.body.scrollHeight)}>Scrolldown</button>
+  //   </div>
+  // )
+
   return (
-    <div className='text-white pb-20'>
-      <div className=''>
-        {chats && chats.map((chat, index) => (
-          <div key={index}>
-            {chat.type == TextType.QUESTION? <Question text={chat.text} />: <Answer text={chat.text} />}
+    <div className='relative max-h-screen overflow-y-hidden'>
+      <div className='h-screen flex flex-row items-stretch'>
+        <div className='bg-white text-black flex flex-col items-center w-1/5'>
+          <div className='flex flex-col items-center gap-y-10 pt-5'></div>
+        </div>
+        <div className='bg-violet-700 text-white w-full overflow-y-scroll relative'>
+          <div>
+            {chats.length ? <div className='h-screen' ref={chatsContainer}>
+              {chats && chats.map((chat, index) => (
+                <div key={index}>
+                  {chat.type == TextType.QUESTION ? <Question text={chat.text} /> : <Answer text={chat.text} />}
+                </div>
+              ))}
+            </div> : <div className='container mx-auto flex flex-col items-center justify-center h-screen w-full text-center'>
+              <i className="bi bi-robot text-3xl mb-4"></i>
+              <h1 className='text-2xl font-bold'>Welcome to NeuralBot</h1>
+              <h1 className='text-2xl font-bold'>You can ask anything here</h1>
+              <span className='mt-4'>Made with ❤️ by Nafis Handoko</span>
+            </div>}
           </div>
-        ))}
-      </div>
-      <div className='fixed bottom-5 w-full flex flex-col items-center'>
-        <form onSubmit={handleSubmit} className="relative w-3/5">
-          {/* <div className="flex absolute inset-y-0 left-0 items-center pl-5 pointer-events-none">
-            <i className="bi bi-question-circle-fill text-violet-700"></i>
-          </div> */}
-          <input value={input} onChange={(e) => setInput(e.target.value)} type="text" id="simple-search" className="shadow-lg bg-white text-violet-700 border border-violet-600 text-sm rounded-lg focus:ring-violet-700 focus:border-violet-700 block w-full px-5 p-2.5 placeholder-gray-500" placeholder="Ask a question..." />
-          <button type='submit' className="flex absolute inset-y-0 right-0 items-center pr-5 cursor-pointer">
-            <i className="bi bi-send-fill text-violet-700"></i>
-          </button>
-        </form>
+          <div className='sticky left-0 bottom-5 w-full flex flex-col items-center'>
+            <form onSubmit={testSubmit} className="relative w-2/3">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-5 pointer-events-none">
+                <i className="bi bi-question-circle-fill text-violet-700"></i>
+              </div>
+              <input value={input} onChange={(e) => setInput(e.target.value)} type="text" id="simple-search" className="shadow-lg bg-white text-violet-700 border border-violet-600 text-sm rounded-lg focus:ring-violet-700 focus:border-violet-700 block w-full px-14 p-2.5 placeholder-gray-500" placeholder="Ask a question..." />
+              <button type='submit' className="flex absolute inset-y-0 right-0 items-center pr-5 cursor-pointer">
+                <i className="bi bi-send-fill text-violet-700"></i>
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   )
